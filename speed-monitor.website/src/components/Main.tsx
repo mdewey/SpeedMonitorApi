bed3d19afa60de9119e7e202537e21b32c5897cf
Mark Dewey <mtdewey55@gmail.com>
2025-01-01 19:54:45 -0500

    feat: integrate Victory chart for visualizing speed data and update query for speed API

--- speed-monitor.website/src/components/Main.tsx
+++ speed-monitor.website/src/components/Main.tsx
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query"
-console.log(import.meta.env) // "123"
 
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryTheme,
+} from "victory";
 
 export default function Main() {
   const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ['token'],
+    queryKey: ['speed-data'],
     queryFn: async () => {
       const response = await fetch(
         `${import.meta.env.VITE_API_URL}/api/v1/token`, {
@@ -16,19 +20,38 @@ export default function Main() {
           api_key: import.meta.env.VITE_API_KEY,
         }),
       });
-      return await response.json()
+      const { data } = await response.json();
+      const dataResponse = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/v1/speed`, {
+        headers: {
+          Authorization: `Bearer ${data.token}`,
+        },
+      });
+      return dataResponse.json();
     },
   })
 
   if (isPending) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
+  console.log({ data })
 
   return (
     <div>
       <div>{isFetching ? 'Updating...' : ''}</div>
-      <div>
-        {data.data.token}
+      <div style={{ backgroundColor: 'white', padding: '20px' }}>
+        <VictoryChart
+          theme={VictoryTheme.clean}
+        >
+          <VictoryLine
+            interpolation="linear"
+            data={data.data.points.map((point: { timestamp: any; downloadSpeed: any; }) => {
+              return {
+                x: point.timestamp,
+                y: point.downloadSpeed,
+              }
+            })} />
+        </VictoryChart>
       </div>
     </div>
   )
